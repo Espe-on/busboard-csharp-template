00739091b0541f81819abdacabb1ddfa21cd93d4
espe-on <eljoe0@googlemail.com>
2020-02-10 15:50:55 +0000

    Created Arrival Details and described class

---
+++ BusBoard.ConsoleApp/ArrivalDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace BusBoard
+{
+    public class ArrivalDetails
+    {
+        public string StationName { get; set; }
+        public string DestinationName { get; set; }
+        public string LineName { get; set; }
+        public int TimeToStation { get; set; }
+    }
+}
