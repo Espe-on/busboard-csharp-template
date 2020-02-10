603d1a5f790b8621e8351214755c5f783d8f7be4
espe-on <eljoe0@googlemail.com>
2020-02-10 15:52:42 +0000

    Jsonretiver now performs more specific task of retriving TFL details

--- BusBoard.ConsoleApp/JsonRetriever.cs
+++ BusBoard.ConsoleApp/JsonRetriever.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -6,12 +7,12 @@ namespace BusBoard
 {
     public class JsonRetriever
     {
-        public static IRestResponse GetJson(string baseUrl, string username, string password, string requestPath)
+        public static List<ArrivalDetails> GetJson(string baseUrl, string requestPath)
         {
             var client = new RestClient(baseUrl); // directs client to TFL
-            client.Authenticator = new HttpBasicAuthenticator(username, password);
             var request = new RestRequest(requestPath, DataFormat.Json); // sets format and type of data acquired
-            var response = client.Get(request); //pulls in response
+            var rawResponse = client.Get<List<ArrivalDetails>>(request); //pulls in response
+            var response = rawResponse.Data;
             return response;
         }
     }
