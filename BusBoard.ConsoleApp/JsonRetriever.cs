eedfce6e2ffe74cc64012f7b7c1b9af4f6aade2b
espe-on <eljoe0@googlemail.com>
2020-02-10 14:32:23 +0000

    class that retrives json files from API

---
+++ BusBoard.ConsoleApp/JsonRetriever.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace BusBoard
+{
+    public class JsonRetriever
+    {
+        public static IRestResponse GetJson(string baseUrl, string username, string password, string requestPath)
+        {
+            var client = new RestClient(baseUrl); // directs client to TFL
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
+            var request = new RestRequest(requestPath, DataFormat.Json); // sets format and type of data acquired
+            var response = client.Get(request); //pulls in response
+            return response;
+        }
+    }
+}
