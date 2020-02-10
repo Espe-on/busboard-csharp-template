160489b38f2703bf600d12b8594dc5c3a5939c5b
espe-on <eljoe0@googlemail.com>
2020-02-10 14:31:13 +0000

    created ApiDetails class, with contructor

---
+++ BusBoard.ConsoleApp/ApiDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace BusBoard
+{
+    public class ApiDetails
+    {
+        public string Username;
+        public string Password;
+        public string Url;
+        public string RequestPath;
+
+        public ApiDetails(string username, string password, string url, string requestPath)
+        {
+            Username = username;
+            Password = password;
+            Url = url;
+            RequestPath = requestPath;
+        }
+    }
+}
