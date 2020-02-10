4b23b97ee784516a56479cae1cf3a7fffb684dfb
espe-on <eljoe0@googlemail.com>
2020-02-10 15:51:46 +0000

    printer now prints from list

--- BusBoard.ConsoleApp/ArrivalsPrinter.cs
+++ BusBoard.ConsoleApp/ArrivalsPrinter.cs
@@ -1,7 +1,19 @@
-﻿namespace BusBoard
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusBoard
 {
-    public class ArrivalsPrinter
+    public class ArrivalsPrinter 
     {
-        
+        public static void Print(List<ArrivalDetails>arrivalsList)
+        {
+            Console.WriteLine($"The station is {arrivalsList[0].StationName}");
+            foreach (var bus in arrivalsList)
+            {
+                Console.WriteLine($"The {bus.LineName} " +
+                                  $"towards {bus.DestinationName} " +
+                                  $"will arrive in {bus.TimeToStation} seconds.");
+            }
+        }
     }
 }
