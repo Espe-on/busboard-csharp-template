1ce64112d3b51440d0a41bc628f8a38c07569666
espe-on <eljoe0@googlemail.com>
2020-02-11 11:32:03 +0000

    removed constructor as not needed

--- BusBoard.ConsoleApp/PostcodeDetails.cs
+++ BusBoard.ConsoleApp/PostcodeDetails.cs
@@ -4,11 +4,5 @@
     {
         public string Longitude { get; set; }
         public string Latitude { get; set; }
-
-        public PostcodeDetails(string longitude, string latitude)
-        {
-            Longitude = longitude;
-            Latitude = latitude;
-        }
     }
 }
