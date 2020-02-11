bd2dbe2519a06a6dbacf0a1daf30fbf99a3275b8
espe-on <eljoe0@googlemail.com>
2020-02-11 12:46:46 +0000

    Postcode Converter now returns lon/lat from postcode

--- BusBoard.ConsoleApp/PostcodeConverter.cs
+++ BusBoard.ConsoleApp/PostcodeConverter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RestSharp;
+using RestSharp.Serialization.Xml;
 
 namespace BusBoard
 {
@@ -11,8 +13,8 @@ namespace BusBoard
             bool response = false; 
             string baseUrl = "https://api.postcodes.io/";
             string requestPath = $"postcodes/{postcode}/validate";
-            var client = new RestClient(baseUrl); // directs client to TFL
-            var request = new RestRequest(requestPath, DataFormat.Json); // sets format and type of data acquired
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(requestPath, DataFormat.Json);
             var rawResponse = client.Get(request).Content;
             if (rawResponse.Contains("true"))
             {
@@ -23,6 +25,28 @@ namespace BusBoard
                 response = false;
             }
             return response; 
+        }
+
+        public static string LonLat(string postcode)
+        {
+            if (PostcodeValidator(postcode) == true)
+            {
+                string baseUrl = "https://api.postcodes.io/";
+                string requestPath = $"postcodes/{postcode}";
+                var client = new RestClient(baseUrl);
+                var request = new RestRequest(requestPath, DataFormat.Json);
+                var rawResponse = client.Get<Return>(request);
+                var postcodeDetails = rawResponse.Data.Result;
+                string reponse = $"{postcodeDetails.Longitude},{postcodeDetails.Latitude}";
+                return reponse;
+            }
+            else
+            {
+                string response = "ERROR INVALID POSTCODE";
+                //put in better errors here
+            }
+
+            return ":(";
         }
     }
 }
--- BusBoard.ConsoleApp/PostcodeDetails.cs
+++ BusBoard.ConsoleApp/PostcodeDetails.cs
@@ -1,5 +1,13 @@
-﻿namespace BusBoard
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace BusBoard
 {
+    public class Return
+    {
+        public int Status { get; set; }
+        public PostcodeDetails Result { get; set; }
+    }
     public class PostcodeDetails
     {
         public string Longitude { get; set; }
