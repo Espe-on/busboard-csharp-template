b45ce13547c1442a1011f5dedfd76b1c40c1c6a0
espe-on <eljoe0@googlemail.com>
2020-02-11 11:16:47 +0000

    added private method to validate postcodes using api

---
+++ BusBoard.ConsoleApp/PostcodeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace BusBoard
+{
+    public class PostcodeConverter
+    {
+        private static bool PostcodeValidator(string postcode)
+        {
+            bool response = false; 
+            string baseUrl = "https://api.postcodes.io/";
+            string requestPath = $"postcodes/{postcode}/validate";
+            var client = new RestClient(baseUrl); // directs client to TFL
+            var request = new RestRequest(requestPath, DataFormat.Json); // sets format and type of data acquired
+            var rawResponse = client.Get(request).Content;
+            if (rawResponse.Contains("true"))
+            {
+                response = true;
+            }
+            else
+            {
+                response = false;
+            }
+            return response; 
+        }
+    }
+}
