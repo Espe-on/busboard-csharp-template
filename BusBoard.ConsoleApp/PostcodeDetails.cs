0f57d595d5f9258239c18367afd378a766ec3dad
espe-on <eljoe0@googlemail.com>
2020-02-11 11:19:26 +0000

    initalised class postcodedetails for storing details of postcodes

---
+++ BusBoard.ConsoleApp/PostcodeDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace BusBoard
+{
+    public class PostcodeDetails
+    {
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
+        public PostcodeDetails(string longitude, string latitude)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+    }
+}
