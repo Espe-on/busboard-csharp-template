54fff884e019ff6c99f9899659a0541a6d3734b6
espe-on <eljoe0@googlemail.com>
2020-02-11 17:27:29 +0000

    initalising postcodeioclient, which is intended to replace postcode converter

---
+++ BusBoard.ConsoleApp/PostcodeIoClient.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+
+namespace BusBoard
+{
+    public class PostcodeIoClient
+    {
+        private readonly RestClient _client;
+
+        public PostcodeIoClient()
+        {
+            _client = new RestClient("https://api.postcodes.io/");
+        }
+
+        private bool ValidatePostCode (string postcode)
+        {
+            bool response = false;
+            var request = new RestRequest($"postcodes/{postcode}/validate");
+            var rawResponse = _client.Get(request).Content;
+            if (rawResponse.Contains("true"))
+            {
+                response = true;
+            }
+
+            return response;
+        }
+    }
+}
