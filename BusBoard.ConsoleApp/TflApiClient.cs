99b3c147b6a0c08d60703fefb9945ef32f07ca35
espe-on <eljoe0@googlemail.com>
2020-02-11 16:51:01 +0000

    Combined ArrivalsJsonRetriver and StationID into TflApiClient

---
+++ BusBoard.ConsoleApp/TflApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+
+namespace BusBoard
+{
+    public class TflApiClient
+    {
+        private readonly RestClient _client;
+        private readonly string _appId;
+        private readonly string _appKey;
+        
+        public TflApiClient()
+        {
+            _client = new RestClient("https://api.tfl.gov.uk/");
+            _appId = Environment.GetEnvironmentVariable("TFL_APP_ID");
+            _appKey = Environment.GetEnvironmentVariable("TFL_KEY");
+        }
+        
+        public IEnumerable<ArrivalDetails> FetchArrivalDetails(string stationId)
+        {
+            var request = new RestRequest($"StopPoint/{stationId}/Arrivals");
+            var response = MakeGetRequest<List<ArrivalDetails>>(request);
+
+            return response
+                .OrderBy(rawResponse => rawResponse.TimeToStation)
+                .Take(5);
+        }
+
+        public List<StationIdDetails> StationIdRetrieve(string latitude, string longitude)
+        {
+            var request = new RestRequest($"StopPoint")
+                .AddQueryParameter("stopTypes", "NaptanPublicBusCoachTram")
+                .AddQueryParameter("radius", "200")
+                .AddQueryParameter("lat", latitude)
+                .AddQueryParameter("lon", longitude);
+             
+            var response = MakeGetRequest<StationIdInitialReturn>(request);
+            return response.StopPoints;
+        }
+
+        private T MakeGetRequest<T>(IRestRequest request) where T : new()
+        {
+            request
+                .AddQueryParameter("app_id", _appId)
+                .AddQueryParameter("app_key", _appKey);
+            return _client.Get<T>(request).Data;
+        }
+    }
+}
