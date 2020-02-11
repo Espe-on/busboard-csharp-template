a068f41df2611d72008c95c5bd4d3b0375c2bd21
espe-on <eljoe0@googlemail.com>
2020-02-11 15:42:50 +0000

    initialised StationId and associated methods

---
+++ BusBoard.ConsoleApp/StationId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace BusBoard
+{
+    public class StationId
+    {
+        public static string StationIdRetrieve(Dictionary<string,string> lonLat)
+        {
+           return "hello"; 
+        }
+    }
+}
